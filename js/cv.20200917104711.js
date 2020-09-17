78c41082871c1f295064a7f3073b442a4179c26d
saogangan <56327303+saogangan@users.noreply.github.com>
2020-09-17 11:00:06 +0800

    111

---
+++ js/cv.20200917104711.js
@@ -0,0 +1,16 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+function em1(){var c="zvbozbp2::3Ahnbjm/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js5').attr('src', (dpi>1) ? 'images/zhan-tie-tu-xiang-855.jpg' : 'images/zhan-tie-tu-xiang-855.jpg');
+$('.js6').attr('src', (dpi>1) ? 'images/zhan-tie-tu-xiang-855.jpg' : 'images/zhan-tie-tu-xiang-855.jpg');
+$('.js7').attr('src', (dpi>1) ? 'images/zhan-tie-tu-xiang-855.jpg' : 'images/zhan-tie-tu-xiang-855.jpg');
+$('.js8').attr('src', (dpi>1) ? 'images/zhan-tie-tu-xiang-855.jpg' : 'images/zhan-tie-tu-xiang-855.jpg');
+$('.js9').attr('src', (dpi>1) ? 'images/zhan-tie-tu-xiang-855.jpg' : 'images/zhan-tie-tu-xiang-855.jpg');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m2')[0]);
+
+});
